daf76a7b5d3443f8d5abe1381a7181a22d6d391f
FrazShabbir <fraz.shabbir54@gmail.com>
2020-10-23 23:23:08 +0500

    touchups

--- sendsms.py
+++ sendsms.py
@@ -1,5 +1,5 @@
 from twilio.rest import Client
-
+#twillio API
 client=Client("AC5d5beb0fc0613ec610a58767032c1c1a","02c062727d389510cf196c258c468d3e")
 
 client.messages.create(to='+923342851998',from_='+12057970697',body="hello!")
