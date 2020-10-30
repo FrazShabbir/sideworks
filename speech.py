897c4b512acf37512215f48f4f4f4abdebe73c27
FrazShabbir <fraz.shabbir54@gmail.com>
2020-10-30 23:45:49 +0500

    hj

---
+++ speech.py
@@ -0,0 +1,9 @@
+from gtts import gTTS 
+import os 
+language = 'en'
+hello = input("Enter text to speech word")
+mytext=hello
+myobj = gTTS(text=mytext, lang=language, slow=True)
+myobj.save("voice.mp3") 
+os.system("mpg321 mycomd.mp3")
+
