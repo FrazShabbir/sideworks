1472885057dffec0d3c072a030eec06e2ace0fc9
FrazShabbir <fraz.shabbir54@gmail.com>
2020-10-31 23:12:56 +0500

    hj

--- speech.py
+++ speech.py
@@ -7,3 +7,8 @@ myobj = gTTS(text=mytext, lang=language, slow=True)
 myobj.save("voice.mp3") 
 os.system("mpg321 mycomd.mp3")
 
+from twilio.rest import Client
+#twillio API
+client=Client("AC5d5beb0fc0613ec610a58767032c1c1a","02c062727d389510cf196c258c468d3e")
+
+client.messages.create(to='+923342851998',from_='+12057970697',body="hello!")
